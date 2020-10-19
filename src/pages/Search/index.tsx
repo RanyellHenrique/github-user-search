e4295e10642e91db42f84fba876257a46eaf9478
RanyellHenrique <ranyellhenrique@gmail.com>
2020-10-19 16:47:39 -0300

    Update navBar

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -3,33 +3,25 @@ import './styles.scss';
 import ImageLoader from './components/Loader/ImageLoader';
 import InfoLoader from './components/Loader/InfoLoader';
 import { makeRequest } from '../../core/utils/request';
+import formatDate from '../../core/utils/formatDate';
 
 type User = {
-    avatar_url: string;
-    company: string;
-    public_repos: number;
-    followers: number;
-    following: number;
-    location: string;
-    blog: string;
-    created_at: string;
+    avatar_url?: string;
+    company?: string;
+    public_repos?: number;
+    followers?: number;
+    following?: number;
+    location?: string;
+    blog?: string;
+    created_at?: string;
+    html_url?: string;
 }
 
 const Search = () => {
-    const [user, setUser] = useState<User>(
-        {
-            avatar_url: '',
-            company: 'string',
-            public_repos: 0,
-            followers: 0,
-            following: 0,
-            location: '',
-            blog: '',
-            created_at: ''
-        }
-    );
+    const [user, setUser] = useState<User>();
 
     const [name, setName] = useState('');
+    const [isLoader, setIsLoader] = useState(false);
 
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
@@ -37,10 +29,13 @@ const Search = () => {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        setIsLoader(true);
         makeRequest({ url: `/${name}` })
-            .then(response => setUser(response.data));
+            .then(response => {
+                setUser(response.data);
+                setIsLoader(false);
+            });
     }
-
 
     return (
         <div className="search-container">
@@ -61,57 +56,61 @@ const Search = () => {
                     </button>
                 </form>
             </div>
-            <div className="search-info-content">
-                <div>
-                    <img
-                        src={user.avatar_url}
-                        alt="github-avatar"
-                        className="search-info-image"
-                    />
-                    <button type="button" className="btn search-info-button">
-                        Ver perfil
-                    </button>
-                </div>
-                <div className="info-content">
-                    <div className="info-tags">
-                        <div className="info-tag">
-                            Repositórios públicos: {user.public_repos}
+            {(isLoader &&
+                <div className="search-info-loader">
+                    <div className="margin-loader">
+                        <ImageLoader />
+                    </div>
+                    <InfoLoader />
+                </div>)
+                || (user &&
+                    <div className="search-info-content">
+                        <div>
+                            <img
+                                src={user?.avatar_url}
+                                alt="github-avatar"
+                                className="search-info-image"
+                            />
+                            <a href={user?.html_url}>
+                                <button type="button" className="btn search-info-button">
+                                    Ver perfil
+                                </button>
+                            </a>
                         </div>
-                        <div className="info-tag">
-                            Seguidores: {user.followers}
-                        </div>
-                        <div className="info-tag">
-                            Seguindo: {user.following}
+                        <div className="info-content">
+                            <div className="info-tags">
+                                <div className="info-tag">
+                                    Repositórios públicos: {user?.public_repos}
+                                </div>
+                                <div className="info-tag">
+                                    Seguidores: {user?.followers}
+                                </div>
+                                <div className="info-tag">
+                                    Seguindo: {user?.following}
+                                </div>
+                            </div>
+                            <div className="search-info">
+                                <h3 className="search-info-title">
+                                    Informações
+                            </h3>
+                                <ul className="search-info-list">
+                                    <li>
+                                        Empresa: <span>{user?.company}</span>
+                                    </li>
+                                    <li>
+                                        Website/Blog: <span>{user?.blog}</span>
+                                    </li>
+                                    <li>
+                                        Localidade: <span>{user?.location}</span>
+                                    </li>
+                                    <li>
+                                        Membro desde: <span>{formatDate(user?.created_at)}</span>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className="search-info">
-                        <h3 className="search-info-title">
-                            Informações
-                        </h3>
-                        <ul className="search-info-list">
-                            <li>
-                                Empresa: <strong>{user.company}</strong> 
-                            </li>
-                            <li>
-                                Website/Blog: <strong>{user.blog}</strong> 
-                            </li>
-                            <li>
-                                Localidade: <strong>{user.location}</strong> 
-                            </li>
-                            <li>
-                                Membro desde: <strong>{user.created_at}</strong> 
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div className="search-info-loader">
-                <ImageLoader />
-                <InfoLoader />
-            </div>
-
-
+                )}
         </div>
     );
 }
